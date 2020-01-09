0ad4875efdd6cfa54790ead55b82a52ff6186c9a
AndrewVitrenko <57104862+AndrewVitrenko@users.noreply.github.com>
2020-01-09 16:19:33 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
-const requestURL = 'https://jsonplaceholder.typicode.com/users'
-
-function f() {
+async function logIn() {
 	let email = document.getElementById('email').value;
 	let password = document.getElementById('password').value;
-	let success = document.getElementById('sucess');
+	let success = document.getElementById('success');
 	let error = document.getElementById('error');
+	window.users;
 
-	let users = fetch(requestURL).then(response => {
-		return response.json();
-	});
+	function getDB() {
+		return fetch('http://localhost:3000/users').then(response => {
+			return response.json()
+		})
+	}
+
+	let user = await getDB().then(data => {
+		users = data		
+	})
+	
+	let emails = [];
 
 	for (let i = 0; i < users.length; i++) {
-		
+		emails.push(users[i]['email'])
 	}
+
+	if (!emails.includes(email)) {
+		error.style.visibility = 'visible'
+		success.style.visibility = 'hidden'
+		error.innerHTML = 'Oopps... Your email or password is incorrect. Please try again'
+	}
+	else {
+		for (let i = 0; i < users.length; i++) {
+			if (users[i]['email'] == email) {
+				if (users[i]['password'] == password) {
+					success.style.visibility = 'visible';
+					error.style.visibility = 'hidden';
+					success.innerHTML = 'You successfuly logged in';
+					error.innerHTML = ''
+				}
+				else {
+					error.style.visibility = 'visible'
+					success.style.visibility = 'hidden'
+					error.innerHTML = 'Oopps... Your email or password is incorrect. Please try again'
+				}	
+			}
+		}
+	}	
 }
